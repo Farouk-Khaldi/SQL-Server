05d34527acbbb611182dd1fbdbd65ffcfa82d09a
Farouk-Khaldi <146712047+Farouk-Khaldi@users.noreply.github.com>
2024-03-12 11:31:48 +0100

    Update volumetrie_tables.sql

--- volumetrie_tables.sql
+++ volumetrie_tables.sql
@@ -1,3 +1,4 @@
+--KB----
 SELECT t.NAME AS TableName, s.Name AS SchemaName, p.rows AS RowCounts,
 SUM(a.total_pages) * 8 AS TotalSpaceKB, SUM(a.used_pages) * 8 AS UsedSpaceKB,
 (SUM(a.total_pages) - SUM(a.used_pages)) * 8 AS UnusedSpaceKB
@@ -9,3 +10,16 @@ LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id
 WHERE t.NAME NOT LIKE 'dt%' AND t.is_ms_shipped = 0 AND i.OBJECT_ID > 255
 GROUP BY t.Name, s.Name, p.Rows
 ORDER BY UsedSpaceKB DESC
+
+---Mb-------
+SELECT t.NAME AS TableName, s.Name AS SchemaName, p.rows AS RowCounts,
+(SUM(a.total_pages) * 8)/1024 AS TotalSpaceMb, (SUM(a.used_pages) * 8)/1024 AS UsedSpaceMb,
+((SUM(a.total_pages) - SUM(a.used_pages)) * 8)/1024 AS UnusedSpaceMb
+FROM sys.tables t
+INNER JOIN sys.indexes i ON t.OBJECT_ID = i.object_id
+INNER JOIN sys.partitions p ON i.object_id = p.OBJECT_ID AND i.index_id = p.index_id
+INNER JOIN sys.allocation_units a ON p.partition_id = a.container_id
+LEFT OUTER JOIN sys.schemas s ON t.schema_id = s.schema_id
+WHERE t.NAME NOT LIKE 'dt%' AND t.is_ms_shipped = 0 AND i.OBJECT_ID > 255
+GROUP BY t.Name, s.Name, p.Rows
+ORDER BY UsedSpaceMb DESC
